a990244e2c566eda36094edbd09e8586ebe8eb52
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-06-06 10:40:50 +0530

    started with posting data

--- client/index.js
+++ client/index.js
@@ -1,5 +1,10 @@
 const parentDiv = document.getElementById("parent-div");
+const titleInput=document.getElementById('titleInput');
+const authorInput=document.getElementById('authorInput');
+const addButton=document.getElementById('addButton');
+
 window.addEventListener("load", getData);
+
 let initialData = [];
 
 async function getData() {
@@ -8,29 +13,59 @@ async function getData() {
   console.log(initialData);
 
   renderData();
+  
 }
 
-async function renderData(){
-    initialData.forEach((data, index) => {
-        const manga = document.createElement("div");
-        const childDivTitle = document.createElement("p");
-        const childDivAuthor = document.createElement("p");
-        const deleteButton=document.createElement('button');
-    
-        childDivTitle.innerText = data.title;
-        childDivAuthor.innerText = data.author;
-        deleteButton.innerText="Delete";
+async function renderData() {
+  initialData.forEach((data, index) => {
+    const manga = document.createElement("div");
+    const childDivTitle = document.createElement("p");
+    const childDivAuthor = document.createElement("p");
+    const deleteButton = document.createElement("button");
 
-        deleteButton.addEventListener('click',async()=>{
-            const deleteData=await axios.delete(`http://localhost:3000/${data.title}`)
-               return deleteData;
-        });
-    
-        manga.appendChild(childDivTitle);
-        manga.appendChild(childDivAuthor);
-        manga.appendChild(deleteButton);
-        parentDiv.appendChild(manga);
-      });
+    childDivTitle.innerText = data.title;
+    childDivAuthor.innerText = data.author;
+    deleteButton.innerText = "Delete";
+
+    deleteButton.addEventListener("click", async () => {
+      const deleteData = await axios.delete(
+        `http://localhost:3000/${data.title}`
+      );
+      return deleteData;
+    });
+
+    manga.appendChild(childDivTitle);
+    manga.appendChild(childDivAuthor);
+    manga.appendChild(deleteButton);
+    parentDiv.appendChild(manga);
+  });
 }
 
 
+
+const addData=async(e)=>{
+ const title= titleInput.value;
+ const author=authorInput.value;
+
+ const data={
+    title:title,
+    author:author
+ }
+
+ 
+
+ 
+    try{
+    await axios.post('http://localhost:3000',data),
+   { headers: {
+        'Content-Type': 'application/json', // Adjust the content type if needed
+      }}}
+    catch(err){
+        console.log(err);
+        console.log(data)
+    }
+ 
+
+}
+
+addButton.addEventListener('click',addData)
