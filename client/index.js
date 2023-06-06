6e56fc647a4e88e5f593291b484ea301a8067b61
Taherpatrawala <109360109+Taherpatrawala@users.noreply.github.com>
2023-06-06 09:46:16 +0530

    Added read and delete functionalities

--- client/index.js
+++ client/index.js
@@ -1,10 +1,36 @@
+const parentDiv = document.getElementById("parent-div");
+window.addEventListener("load", getData);
+let initialData = [];
 
+async function getData() {
+  const data = await axios.get("http://localhost:3000");
+  initialData = await data.data;
+  console.log(initialData);
 
-const parentDiv=document.getElementById("parent-div")
-
-async function getData(){
-const data= await axios.get('http://localhost:3000');
-return console.log(data.data)
+  renderData();
 }
 
-parentDiv.addEventListener("click",getData)
+async function renderData(){
+    initialData.forEach((data, index) => {
+        const manga = document.createElement("div");
+        const childDivTitle = document.createElement("p");
+        const childDivAuthor = document.createElement("p");
+        const deleteButton=document.createElement('button');
+    
+        childDivTitle.innerText = data.title;
+        childDivAuthor.innerText = data.author;
+        deleteButton.innerText="Delete";
+
+        deleteButton.addEventListener('click',async()=>{
+            const deleteData=await axios.delete(`http://localhost:3000/${data.title}`)
+               return deleteData;
+        });
+    
+        manga.appendChild(childDivTitle);
+        manga.appendChild(childDivAuthor);
+        manga.appendChild(deleteButton);
+        parentDiv.appendChild(manga);
+      });
+}
+
+
